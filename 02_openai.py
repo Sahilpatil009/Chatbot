7a5a0ba0fbca8d6e2781354a4a6927298fe85372
Sahil Patil <173548370+Sahilpatil009@users.noreply.github.com>
2026-03-19 14:13:28 +0530

    Refactor OpenAI client setup and command handling
    Removed chat history and cleaned up code structure.

--- 02_openai.py
+++ 02_openai.py
@@ -3,24 +3,13 @@ from openai import OpenAI
 # pip install openai
 # if you save the key under a different enviroment variable name, you can do something like:
 client = OpenAI(
-  api_key="sk-proj--Wx17ehGk2PnwmzCHcDwT3BlbkFJMj6bYTk9jG1bqZaFTcj", # Enter Api key please
+  api_key="", # Enter Api key please
 
 )
 
 command = '''
 
-[6:01 PM, 10/31/2024] Sahil: Hi
-[6:01 PM, 10/31/2024] Sis: Kya
-[6:01 PM, 10/31/2024] Sahil: Kahi nahi
-[6:01 PM, 10/31/2024] Sis: Bye
-[6:01 PM, 10/31/2024] Sahil: Bye
-[6:01 PM, 10/31/2024] Sahil: Good night
-[6:01 PM, 10/31/2024] Sis: Good night
-[6:02 PM, 10/31/2024] Sis: What about your coding ?
-[6:02 PM, 10/31/2024] Sahil: Quite good
-[6:02 PM, 10/31/2024] Sis: Ok
-[6:02 PM, 10/31/2024] Sahil: Bye
-[6:02 PM, 10/31/2024] Sis: Bye
+
 
 
 
